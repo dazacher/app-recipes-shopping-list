45f2c73d3bf850b3717c33ffb3db1f290c069bda
dazacher <dazacher2000@yahoo.com>
2021-06-16 06:43:13 -0700

    Added header componenet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
