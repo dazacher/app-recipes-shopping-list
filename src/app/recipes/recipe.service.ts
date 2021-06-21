fecb85b6525b36cc57e1f6065f45fe79b961315a
dazacher <dazacher2000@yahoo.com>
2021-06-21 12:40:55 -0700

    Added recipe items to recipe-detail.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://easychickenrecipes.com/wp-content/uploads/2019/03/chicken-white-wine-sauce-4-of-8-599x860.jpg'),
-        new Recipe('Recipe 2 Test', 'This is a test', 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/09/03/20/turmeric-chicken-kebabs.jpg?width=640&auto=webp&quality=75')
+        new Recipe('Grilled Chicken Breast', 
+                    'This simple grilled chicken breast recipe is seriously the best you will try! Coated in spices, it’s so perfectly juicy and charred. Get all my top tips and learn how to grill the most amazing chicken!', 
+                    'https://easychickenrecipes.com/wp-content/uploads/2020/06/grilled-chicken-recipe-best-5-of-6-768x1152.jpg',
+                    [
+                        new Ingredient('Chicken Breast', 5),
+                        new Ingredient('Barbeque Sauce', 1)
+                    ]),
+        new Recipe('Cranberry Pork Tenderloing', 
+                    'This is a really easy dinner recipe to make. SUPER EASY!  Whether you cook a lot or are a beginner kind of cook, this Cranberry Pork Tenderloin is a dinner that you will be successful at making and serving. There are just a few ingredients, and it’s simple prep.', 
+                    'https://www.recipegirl.com/wp-content/uploads/2008/09/Cranberry-Pork-Tenderloin-1.jpg',
+                    [
+                        new Ingredient('Pork Tenderloin', 6),
+                        new Ingredient('Cherry Tomatoes', 10)
+                    ])
       ];
 
       getRecipes() {
